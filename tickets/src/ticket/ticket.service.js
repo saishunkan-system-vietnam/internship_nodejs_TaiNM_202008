fb2b67ef99d8e7ca4ba43f1716e054df3c642540
VN00XX2\ChungPV <phanchung97pro@gmail.com>
2020-08-27 17:33:58 +0700

    fix tickets

--- tickets/src/ticket/ticket.service.js
+++ tickets/src/ticket/ticket.service.js
@@ -188,6 +188,48 @@ export class TicketService {
         //console.log(deleteticket);
     }
 
+    //airport
+    // async selectairport() {
+    //     await this.connectdatabase();
+    //     let airport = await db.getSchema('mydb').getTable('airport');
+    //     let selectairport =   await airport.select().execute()
+    //     console.log(selectairport);
+    //     return selectairport.fetchAll();
+    // }
+    // async insertairport(name) {
+    //     await this.connectdatabase();
+    //     let airport = await db.getSchema('mydb').getTable('airport');
+    //     if (!name || name.trim() === "") {
+    //         throw "name NOT NULL";
+    //     }
+    //     let insert = await airport
+    //                 .insert('name')
+    //                 .values(name)
+    //                 .execute();
+    // }
+    // async updateairport(id,name) {
+    //     await this.connectdatabase();
+    //     let airport = await db.getSchema('mydb').getTable('airport');
+    //     if (!name || name.trim() === "") {
+    //         throw "name NOT NULL";
+    //     }
+    //     let updateairport = await airport
+    //                         .update()
+    //                         .where('id = :param')
+    //                         .bind('param',id)
+    //                         .set('name',name)
+    //                         .execute();
+    // }
+    // async deleteairport(id) {
+    //     await this.connectdatabase();
+    //     let airport = await db.getSchema('mydb').getTable('airport');
+    //     let deleteticket = await airport.delete()
+    //                         .where('id = :param')
+    //                         .bind('param',id)
+    //                         .execute();
+    //     console.log(deleteticket);
+    // }
+
     // beginTransaction() {
     //     if (this.session) {
     //         // Start a transaction
