9c925d56e64ef0958538b1ba3d3bab7c681c5d15
VN00XX2\ChungPV <phanchung97pro@gmail.com>
2020-08-27 08:56:45 +0700

    delete dataservice

--- tickets/src/dataservice/dataservice.module.js
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DataserviceController } from './dataservice.controller';
-import { DataserviceService } from './dataservice.service';
-
-@Module({
-  controllers: [DataserviceController],
-  providers: [DataserviceService],
-})
-export class DataserviceModule {}
