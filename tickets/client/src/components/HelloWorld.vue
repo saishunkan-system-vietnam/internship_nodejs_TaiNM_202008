d1ec661934a95524ee3742638a1cb70bd322d749
phanchung97pro <phanchung97pro@gmail.com>
2020-09-07 23:59:13 +0700

    test

---
+++ tickets/client/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <table border="1">
+    <tr>
+        <th>Họ và tên</th>
+        <th>Năm sinh</th>
+        <th>Giới tính</th>
+        <th>Quê quán</th>
+    </tr>
+    <tr v-for="post in posts" :key="post.id">
+        <td>{{ post[0] }}</td>
+        <td>{{ post[1] }}</td>
+        <td>{{ post[2] }}</td>
+        <td>{{ post[3] }}</td>
+    </tr>
+</table>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: [],
+    };
+  },
+
+  // lấy dữ liệu khis component được tạo thành công
+  created() {
+    axios
+      .get(`http://localhost:3000/ticket`)
+      .then(response => {
+        console.log(response.data.data);
+        this.posts = response.data.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  },
+};
+</script>
--- tickets/src/main.js
+++ tickets/src/main.js
@@ -3,6 +3,7 @@ import { TicketModule } from './ticket/ticket.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TicketModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
