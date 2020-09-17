3ea241b4fe12435e86acdb64bd8df3bc06af39c2
Nguyen Si Hieu <hackeronex@gmail.com>
2020-09-17 17:10:29 +0700

    add sort for table, fix icon

--- my-project/src/components/category/Category.vue
+++ my-project/src/components/category/Category.vue
@@ -5,10 +5,10 @@
     </form>
     <br />
 
-    <table class="table">
+    <table class="table" id="myTable">
       <thead>
         <tr>
-          <th scope="col">Airline Code</th>
+          <th style="cursor: pointer" scope="col" @click="sortTable(0)">Airline Code</th>
           <th scope="col">Airline Name</th>
           <th scope="col">Type of Seat</th>
           <th scope="col">Delete</th>
@@ -25,7 +25,7 @@
               class="btn waves-effect waves-light red darken-2"
               @click="deleteCategory(category.alID, category.sID)"
             >
-              <font-awesome-icon icon="user-secret" />
+              <i style="color: red" class="fas fa-trash-alt"></i>
             </a>
           </td>
         </tr>
@@ -71,13 +71,74 @@ export default {
       this.pageOfItems = filteredItems;
       // console.log(pageOfItems)
     },
+    sortTable(n) {
+      var table,
+        rows,
+        switching,
+        i,
+        x,
+        y,
+        shouldSwitch,
+        dir,
+        switchcount = 0;
+      table = document.getElementById("myTable");
+      switching = true;
+      //Set the sorting direction to ascending:
+      dir = "asc";
+      /*Make a loop that will continue until
+  no switching has been done:*/
+      while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+    first, which contains table headers):*/
+        for (i = 1; i < rows.length - 1; i++) {
+          //start by saying there should be no switching:
+          shouldSwitch = false;
+          /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+          if (dir == "asc") {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              //if so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else if (dir == "desc") {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              //if so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          //Each time a switch is done, increase this count by 1:
+          switchcount++;
+        } else {
+          /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+          if (switchcount == 0 && dir == "asc") {
+            dir = "desc";
+            switching = true;
+          }
+        }
+      }
+    },
   },
   computed: {
     filteredItems() {
       return this.categories.filter((category) => {
         return (
-          category.alName.toLowerCase().indexOf(this.search.toLowerCase()) >
-          -1
+          category.alName.toLowerCase().indexOf(this.search.toLowerCase()) > -1
         );
       });
     },
--- my-project/src/components/category/Table.vue
+++ my-project/src/components/category/Table.vue
@@ -13,7 +13,7 @@
               v-model="input.alCode"
             />
           </div>
-           <div class="form-group col-md-6">
+          <div class="form-group col-md-6">
             <label for="alName">Airline Name</label>
             <input
               type="text"
@@ -25,19 +25,25 @@
           </div>
         </div>
         <button type="submit" class="btn btn-primary" @click="insertAirline(input)">Create Airline</button>
-        <br><br>
+        <br />
+        <br />
       </form>
     </div>
 
-<form class="form-inline">
-    <input type="text" class="form-control " placeholder="Search by Airline Name" v-model="search">
-  </form> <br>
+    <form class="form-inline">
+      <input type="text" class="form-control" placeholder="Search by Airline Name" v-model="search" />
+    </form>
+    <br />
 
     <table class="table">
       <thead>
         <tr>
           <th scope="col">ID</th>
-          <th scope="col">Airline Code</th>
+          <th scope="col">
+            Airline Code
+            <i @click="sortInc" class="fas fa-long-arrow-alt-up"></i>
+            <i @click="sortDec" class="fas fa-long-arrow-alt-down"></i>
+          </th>
           <th scope="col">Airline Name</th>
           <th scope="col">Edit</th>
           <th scope="col">Delete</th>
@@ -54,7 +60,7 @@
               class="btn waves-effect waves-light red darken-2"
               @click="updateAirline(airline.alID)"
             >
-              <font-awesome-icon icon="user-secret" />
+              <i style="color:green" class="fas fa-pen"></i>
             </a>
           </td>
           <td>
@@ -63,16 +69,16 @@
               class="btn waves-effect waves-light red darken-2"
               @click="deleteAirline(airline.alID)"
             >
-              <font-awesome-icon icon="user-secret" />
+              <i style="color: red" class="fas fa-trash-alt"></i>
             </a>
           </td>
         </tr>
       </tbody>
     </table>
 
-     <div class="card-footer pb-0 pt-3">
-            <jw-pagination :items="filteredItems" :pageSize="5" @changePage="onChangePage"></jw-pagination>
-        </div>
+    <div class="card-footer pb-0 pt-3">
+      <jw-pagination :items="filteredItems" :pageSize="5" @changePage="onChangePage"></jw-pagination>
+    </div>
   </div>
 </template>
 
@@ -87,7 +93,7 @@ export default {
       airlines: [],
       pageOfItems: [],
       seats: [],
-      search: ''
+      search: "",
     };
   },
 
@@ -133,21 +139,37 @@ export default {
       DataService.insertAirline(data)
         .then((response) => console.log("success"))
         .catch((e) => console.log(e));
-      this.$router.push('/admin/insertCategory')
-      this.$router.go()
+      this.$router.push("/admin/insertCategory");
+      this.$router.go();
     },
-onChangePage(filteredItems) {
-            // update page of items
-            this.pageOfItems = filteredItems;
-            // console.log(pageOfItems)
-        }
+    onChangePage(filteredItems) {
+      // update page of items
+      this.pageOfItems = filteredItems;
+      // console.log(pageOfItems)
+    },
+    sortDec() {
+      return this.airlines.sort((a, b) =>
+        a.alCode.toLowerCase() > b.alCode.toLowerCase() ? 1 : -1
+      );
+      //       return this.airlines.sort(function(a, b) {
+      //    return a.alCode.toLowerCase().localeCompare(b.alCode.toLowerCase());
+      // });
+    },
+
+    sortInc() {
+      return this.airlines.sort((a, b) =>
+        a.alCode.toLowerCase() < b.alCode.toLowerCase() ? 1 : -1
+      );
+    },
   },
   computed: {
-    filteredItems () {
-      return this.airlines.filter(airline => {
-         return airline.alName.toLowerCase().indexOf(this.search.toLowerCase()) > -1
-      })
-    }
-  }
+    filteredItems() {
+      return this.airlines.filter((airline) => {
+        return (
+          airline.alName.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+        );
+      });
+    },
+  },
 };
 </script>
